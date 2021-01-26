1be77c4df433afbec9bfd4bd384fbb601aeb7f36
hzhu040 <59568256+hzhu040@users.noreply.github.com>
2021-01-25 23:25:48 -0500

    Update on the Web template; Servo motor for sprayer

--- class1.py
+++ class1.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request
-
+from time import sleep
 
 import RPi.GPIO as GPIO # import the RPi library and its GPIO function?? PWM to control the servo motor??
 
@@ -10,51 +10,63 @@ app = Flask(__name__)
 def home():
     TES_pin = 26
     if request.method =="POST":
+        print("has posted")
         userValue = request.form["val"] # get the value of val from html file and store as str
-        SpraySwitch = request.form["Chekbox"]
-        print(userValue)
-        print(SpraySwitch)
-        IntVal = int(userValue)
-        if SpraySwitch == 0:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(TES_pin,GPIO.OUT)
-            print("One")
-            GPIO.output(TES_pin,GPIO.LOW)
-            return render_template("index.html") # Enable off, turn off the sprayer, no motion
-        else:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(TES_pin,GPIO.OUT)
-            GPIO.output(TES_pin,GPIO.HIGH)
-            print("Two")
-            return render_template("on.html") #turn On switch
+        print("Please Turn On the Device First")
+        return render_template("index.html") #turn On switch
     else:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(TES_pin,GPIO.OUT)
         GPIO.output(TES_pin,GPIO.LOW)
-        print("FOUR")
+        print("Pump Off")
         return render_template("index.html") # nothing happen
 
     #Spray Power ON
-@app.route('/on')
+@app.route('/on',methods=["GET","POST"])
 def on():
     RelayControl = 26
     print("Three")
+    if request.method =="POST":
+        userValue = request.form["val"] # get the value of val from html file and store as str
+        print(userValue)
+        SMotor_Control(userValue)
+        return render_template('on.html')
+    else:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(RelayControl,GPIO.OUT)
+        GPIO.output(RelayControl,GPIO.HIGH)
+
+        return render_template('on.html')
+
+
+
+def SMotor_Control(degree):
+    print("Spray Motor Control")
+    val1=SetAngle(degree)
+    servoM1_pin = 6 # PWM pin 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(RelayControl,GPIO.OUT)
-    GPIO.output(RelayControl,GPIO.HIGH)
-    return render_template('on.html')
+    GPIO.setup(servoM1_pin,GPIO.OUT)
+    m1=GPIO.PWM(servoM1_pin,50)
+    m1.start(0)
+    m1.ChangeDutyCycle(12)
+    sleep(4)
+    m1.ChangeDutyCycle(val1)
+    sleep(2)
+    GPIO.cleanup()
 
+    
 
-
-def Spray_Control(power, readback):
-    servoM1_pin = 17 # PWM pin 
-    servoM2_pin = 18
-    GPIO.setup(servoM1_pin, GPIO.OUT)
-    GPIO.setup(servoM2_pin, GPIO.OUT)
-    GPIO.cleanup() # clean up other GIPO assignments
-
-    M1 = GPIO.PWM(servoM1_pin, 50)# GPIO 17 for PWM with 50Hz
-    M2 = GPIO.PWM(servoM2_pin, 50)# -----18 ---
+def SetAngle(angle):
+    angle1 = float(angle)
+    print(angle1)
+    angle1+=90
+    print(angle1)
+    duty = 12-(angle1/180)*10
+    result=int(duty)
+    print(duty)
+    print('Result')
+    print(result)
+    return result
 
 
 
